06515031ba98418e1a69d191ae4612c84bc74e6b
Francisco Hernandez <hernandezf797@gmail.com>
2015-10-21 23:40:43 -0400

    submitting assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$("#Square").click(function(){
+		alert("Thanks for clicking");
+	});
+
+	$("#fade").fadeIn(4000);
+
+	$("#slide").slideUp(6000);
+
+	$("#attr").click(function(){
+		$("#gb").attr("src", "cowboy.gif");
+	});
+});
---
+++ javascriptcheatsheet.js
@@ -0,0 +1,53 @@
+// Variables: Container of information
+// ex: 
+var x = 5
+var y = 6
+var total = x+y
+
+// Debugging ex:
+var x = 5
+var y = 6
+var total = x+y
+console.log(total);
+alert(total);
+
+// Different data types
+Numbers (index, double, float ex: x=5, x=5.5, x=5.5555)
+Strings (characters, strings of character letters ex: "Al" 'Al' "5")
+Booleans (true or flase ex: light on=true)
+
+// Arrays
+Array (list of variables ex: var arr=["M", "T", "W", "TH", "F", 2, 5] or
+Var arr= new array (
+arr [0]="M", (starts with index 0)
+arr [1]="T",
+arr [2]="W",
+arr [3]="TH",
+arr [4]="F",
+to use seperate array ex: Console.log (arr[3]) this will select "TH"
+multi-dimentional array: array within the array
+
+// Logics
+== (5=="5" true)
+!=
+=== (5==="5" flase since one of the fives had qoutations)
+!==
+<
+<=
+>
+>=
+&& (True and True = True; True and False=False; false and false= false)
+|| (True or False=True;)
+
+// Functions
+function sayHello(){
+	return alert("HI WORLD!!");
+}
+
+function stockMultiply(a, b){
+	return console.log(a*b);
+}
+
+var greeting = function (name){
+	console.log("Great to see you," + " " + name);
+}
